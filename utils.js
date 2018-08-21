fb5d2ffc735f978b7ec6fd45f007ced80f5d1af1
bckramer <bckramer97@gmail.com>
2018-08-21 16:57:01 -0500

    Converted headers to JavaScript. Needs to be implemented on every page now. Made the banner transparent. Made mobile v iewing a little better, but can't get the banner to not cover text.

---
+++ utils.js
@@ -0,0 +1,34 @@
+var ul = document.createElement("UL");
+
+createLiElement("About Me", "#picture", ul);
+createLiElement("Significant Projects", "/SignificantProjects.html", ul);
+createLiElement("Professional Experience", "/ProfessionalExperience.html", ul);
+createLiElement("Skills", "/CodingSkills.html", ul);
+createLiElement("Significant Projects", "/SignificantProjects.html", ul);
+
+createLiWithImage("images/linkedin.png", "https://www.linkedin.com/in/benjamin-kramer-579701148/", ul);
+createLiWithImage("images/github.png", "https://github.com/bckramer", ul);
+
+document.body.appendChild(ul);
+
+function createLiElement(title, href, ul) {
+    let li = document.createElement("LI");
+    let a = document.createElement("a");
+    let linkText = document.createTextNode(title);
+    a.appendChild(linkText);
+    a.title = title;
+    a.href = href;
+    li.appendChild(a);
+    ul.appendChild(li);
+}
+
+function createLiWithImage(src, href, ul) {
+    let li = document.createElement("LI");
+    let a = document.createElement("a");
+    let img = document.createElement("img");
+    img.src = src;
+    a.append(img)
+    a.href = href;
+    li.appendChild(a);
+    ul.appendChild(li);
+}
