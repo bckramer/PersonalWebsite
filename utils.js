63e908f2eef9b08130e90a1d0c1867e35435e583
bckramer <bckramer97@gmail.com>
2018-08-27 18:11:37 -0500

    Made some more changes to make the page more dynamic. This includes how far the color change stretches on onhover for the links and how big the main text. This makes the mobile experience far better.

--- utils.js
+++ utils.js
@@ -1,11 +1,10 @@
 var ul = document.createElement("UL");
 ul.setAttribute("id", "ul");
 
-createLiElement("About Me", "#picture", ul);
+createLiElement("About Me", "/index", ul);
 createLiElement("Significant Projects", "/SignificantProjects.html", ul);
 createLiElement("Professional Experience", "/ProfessionalExperience.html", ul);
 createLiElement("Skills", "/CodingSkills.html", ul);
-
 createLiWithImage("images/linkedin.png", "https://www.linkedin.com/in/benjamin-kramer-579701148/", ul);
 createLiWithImage("images/github.png", "https://github.com/bckramer", ul);
 
@@ -14,6 +13,37 @@ document.getElementById("picture").style.marginTop = ul.clientHeight.toString() 
 
 window.onresize = function(event) {
     document.getElementById("picture").style.marginTop = ul.clientHeight.toString() + "px";
+    updateTextSize();
+    updateLiOnHoverSizes();
+};
+
+window.onload = function(event) {
+    document.getElementById("picture").style.marginTop = ul.clientHeight.toString() + "px";
+    updateTextSize();
+    updateLiOnHoverSizes();
+};
+
+function updateTextSize() {
+    let texts = document.getElementsByClassName("center");
+    for (let i = 0; i < texts.length; i++) {
+        texts[i].style.fontSize = (document.getElementById("picture").clientHeight * .70).toString() + "%";
+    }
+}
+
+function updateLiOnHoverSizes() {
+    let liElements = ul.getElementsByTagName("li");
+    let screenWidth = ul.clientWidth;
+    let css;
+    if (screenWidth < 676) {
+        css = 'li a:hover{ height: auto; display: block; }';
+    } else {
+        css = 'li a:hover{ height: ' + ul.clientHeight.toString() + 'px; display: block; }';
+    }
+    let style = document.createElement('style');
+    style.appendChild(document.createTextNode(css));
+    for (let i = 0; i < liElements.length; i++) {
+        liElements[i].appendChild(style);
+    }
 }
 
 function createLiElement(title, href, ul) {
@@ -23,6 +53,7 @@ function createLiElement(title, href, ul) {
     a.appendChild(linkText);
     a.title = title;
     a.href = href;
+    li.height = ul.clientHeight.toString() + "px";
     li.appendChild(a);
     ul.appendChild(li);
 }
@@ -36,4 +67,5 @@ function createLiWithImage(src, href, ul) {
     a.href = href;
     li.appendChild(a);
     ul.appendChild(li);
+    img.zIndex = 5;
 }
