0dbbd4962a11d076ad1088d0677546219f6c2fcf
bckramer <bckramer97@gmail.com>
2018-08-28 16:17:09 -0500

    Added @media tags in style.css to change the size of elements in the header. Removed the top margin. Deleted some unnecessary styles, code etc. Removed the HACKISU picture from the project page.

--- utils.js
+++ utils.js
@@ -9,40 +9,22 @@ createLiWithImage("images/linkedin.png", "https://www.linkedin.com/in/benjamin-k
 createLiWithImage("images/github.png", "https://github.com/bckramer", ul);
 
 document.body.appendChild(ul);
-document.getElementById("picture").style.marginTop = ul.clientHeight.toString() + "px";
+// document.getElementById("picture").style.marginTop = ul.clientHeight.toString() + "px";
 
 window.onresize = function(event) {
-    document.getElementById("picture").style.marginTop = ul.clientHeight.toString() + "px";
+    // document.getElementById("picture").style.marginTop = ul.clientHeight.toString() + "px";
     updateTextSize();
-    updateLiOnHoverSizes();
 };
 
 window.onload = function(event) {
-    document.getElementById("picture").style.marginTop = ul.clientHeight.toString() + "px";
+    // document.getElementById("picture").style.marginTop = ul.clientHeight.toString() + "px";
     updateTextSize();
-    updateLiOnHoverSizes();
 };
 
 function updateTextSize() {
     let texts = document.getElementsByClassName("center");
     for (let i = 0; i < texts.length; i++) {
         texts[i].style.fontSize = (document.getElementById("picture").clientHeight * .70).toString() + "%";
-    }
-}
-
-function updateLiOnHoverSizes() {
-    let liElements = ul.getElementsByTagName("li");
-    let screenWidth = ul.clientWidth;
-    let css;
-    if (screenWidth < 676) {
-        css = 'li a:hover{ height: auto; display: block; }';
-    } else {
-        css = 'li a:hover{ height: ' + ul.clientHeight.toString() + 'px; display: block; }';
-    }
-    let style = document.createElement('style');
-    style.appendChild(document.createTextNode(css));
-    for (let i = 0; i < liElements.length; i++) {
-        liElements[i].appendChild(style);
     }
 }
 
@@ -62,10 +44,10 @@ function createLiWithImage(src, href, ul) {
     let li = document.createElement("LI");
     let a = document.createElement("a");
     let img = document.createElement("img");
+    img.className = "headerPictures";
     img.src = src;
     a.append(img)
     a.href = href;
     li.appendChild(a);
     ul.appendChild(li);
-    img.zIndex = 5;
 }
